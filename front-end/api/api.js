d67dae1f04b49c68da8b5b4f10dcea7efad8f80c
Nilson Hoffmann <netohjunior216@gmail.com>
2025-02-21 22:32:41 -0300

    Ajustes no process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,9 +2,10 @@
 import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = "development";
+//const { NODE_ENV } = process.env;
 
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+//const URL = "http://localhost:3001/api";
+const URL = "https://deploy-jornada-full-stack-spotify.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
